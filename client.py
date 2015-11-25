05b404d60372abccea1603b1d96de1599834f10e
Matt Hammerly <hammerly.matt@gmail.com>
2015-11-24 23:02:44 -0500

    changed magic values to reflect what is on the actual test network

--- client.py
+++ client.py
@@ -3,21 +3,21 @@ import os
 from urllib.request import urlopen
 
 # each node on the network gets one hardcoded account number
-accts = {'10.0.0.1':10293,
-         '10.0.0.2':1293,
-         '10.0.0.3':13333,
-         '10.0.0.4':7466,
-         '10.0.0.5':4137,
+accts = {'10.1.1.1':10293,
+         '10.1.1.2':1293,
+         '10.1.1.3':13333,
+         '10.1.1.4':7466,
+         '10.1.1.5':4137,
          'localhost':19001}
 
-# server_host = '10.0.0.5:9999'
-server_host = 'http://127.0.0.1:9999'
+# server_host = 'http://10.1.1.5:9999'
+server_host = 'http://127.1.1.1:9999'
 
-# f = os.popen('ifconfig eth0 | grep "inet\ addr" | cut -d: -f2 | cut -d" " -f1')
+# f = os.popen('ifconfig eth1 | grep "inet\ addr" | cut -d: -f2 | cut -d" " -f1')
 # ip = f.read()[:-1]
 # f.close()
 
-ip = '10.0.0.1'
+ip = '10.1.1.1'
 
 def main(args):
     if len(args) != 3:
@@ -27,7 +27,6 @@ def main(args):
     amt = args[2]
     data = bytes("src="+str(accts[ip]) + "&dest="+str(accts[dest]) + "&amt="+amt, "ASCII")
     response = urlopen(server_host, data)
-    pass
 
 if __name__ == "__main__":
     main(sys.argv)
